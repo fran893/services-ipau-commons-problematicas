7d18c8b3c3bf32049d0e66387d61dfa634e09b44
Franco <alvarez.fr893@gmail.com>
2020-02-06 13:38:26 -0300

    primer commint commons problematicas principales

---
+++ services-ipau-commons-problematicas/src/test/java/com/app/ipau/commons/problematicas/ServicesIpauCommonsProblematicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ipau.commons.problematicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesIpauCommonsProblematicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
